e6ccd3839260304eaa9dcab3c9f0435606e3f7c9
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2021-10-15 17:13:19 +0200

    scaffolding of the Server

---
+++ buzzard/readme.md
@@ -0,0 +1,15 @@
+#Buzzard
+
+##Goal
+
+Learn as much Rust as possible by implementing a simple web server.
+Pet project.
+
+##Design
+Single threaded server
+
+    Bricks :
+        - TCP Listener
+        - Http Parser
+        - Handler
+
--- buzzard/src/main.rs
+++ buzzard/src/main.rs
@@ -1,3 +1,28 @@
+use log::{info};
+use env_logger::{Builder, Target};
+
 fn main() {
-    println!("Hello, world!");
+    let mut builder = Builder::from_default_env();
+    builder.target(Target::Stdout);
+    builder.init();
+
+    let server = Server::new(String::from("127.0.0.1"), 8080);
+    server.run();
+}
+
+struct Server {
+    address:String,
+    port:u16
+}
+
+impl Server {
+
+    fn new(address:String, port:u16) -> Self {
+        Server{address, port}
+    }
+
+    fn run(&self){
+        info!("Buzzard is running");
+    }
+
 }
